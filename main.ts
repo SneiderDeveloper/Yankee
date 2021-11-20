819d81bb8c09057bbeb93eb6c93ca78c759cde43
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2021-11-19 23:17:54 -0500

    Sonido caida

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onGesture(Gesture.LogoDown, function () {
+    music.playMelody("C5 B A G F E D C ", 150)
     basic.showString("Oh, no. Levántame por favor")
 })
 music.playMelody("C5 C G - - - - - ", 500)
